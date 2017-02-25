a41ea8f7c5c9db46d1e16c74afcc76618b2c14d0
Edgar Flores <eflorespalma@gmail.com>
2017-02-25 10:04:17 -0500

    Implementing a basic Express server.

--- http-server.js
+++ http-server.js
@@ -2,7 +2,6 @@ const http = require('http');
 
 const port = 8085;
 const hostname = '127.0.0.1'
-
 const server = http.createServer((request, response) => {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
